449125d3cd37f0e85a228a5be7679c8ce9e4a6fc
Ander <anderson.bengolea@scanntech.com>
2024-03-28 10:20:58 -0500

    solving expiration token

--- src/app/modules/auth/interceptor/auth.interceptor.ts
+++ src/app/modules/auth/interceptor/auth.interceptor.ts
@@ -1,21 +1,36 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable } from '@angular/core';
 import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Observable, throwError} from 'rxjs';
 import { TokenService } from '../services/token.service';
+import {jwtDecode, JwtPayload} from 'jwt-decode';
+import {SweetAlertService} from "../../../shared/services/sweet-alert.service";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  private tokenService = inject(TokenService);
+
+  constructor(private tokenService: TokenService,
+              private sweetAlertService: SweetAlertService) {
+  }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const token = this.tokenService.getToken();
 
     if(token) {
+      const decodedToken: JwtPayload = jwtDecode(token);
+      const currentTime = Math.floor(Date.now() / 1000);
+
+      if(decodedToken && decodedToken.exp && decodedToken.exp < currentTime) {
+        this.tokenService.clearOnLogout();
+        this.sweetAlertService.infoAlert('Sesión expirada, vuelva a iniciar sesión');
+
+        return throwError(() => new Error('Token expired'));
+      }
+
       const cloneRequest = request.clone({
         setHeaders: {
           Authorization: `Bearer ${token}`
